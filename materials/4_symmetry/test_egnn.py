44d4be8655736a337043db76b690ea406b8550db
ZihangL <91740087+ZihangL@users.noreply.github.com>
2023-03-21 15:54:55 +0800

    add rotation test

--- materials/4_symmetry/test_egnn.py
+++ materials/4_symmetry/test_egnn.py
@@ -3,10 +3,28 @@ import jax.numpy as jnp
 from jax.config import config   
 config.update("jax_enable_x64", True)
 from jax.flatten_util import ravel_pytree
+from functools import partial
 import numpy as np 
 import haiku as hk
 
 from egnn import EGNN
+
+@partial(jax.vmap, in_axes=(0, None))
+def rotate(x, angle):
+    """
+        Euler rotation of x.
+        Input:
+            x.shape: (3,)
+            angle.shape: (3,)
+    """
+    # Create rotation matrices
+    Rx = jnp.array([[1, 0, 0], [0, jnp.cos(angle[0]), -jnp.sin(angle[0])], [0, jnp.sin(angle[0]), jnp.cos(angle[0])]])
+    Ry = jnp.array([[jnp.cos(angle[1]), 0, jnp.sin(angle[1])], [0, 1, 0], [-jnp.sin(angle[1]), 0, jnp.cos(angle[1])]])
+    Rz = jnp.array([[jnp.cos(angle[2]), -jnp.sin(angle[2]), 0], [jnp.sin(angle[2]), jnp.cos(angle[2]), 0], [0, 0, 1]])
+    
+    # Apply rotations z -> y -> x
+    R = jnp.dot(Rz, jnp.dot(Ry, Rx))
+    return jnp.dot(R, x)
 
 def test_egnn():
     depth = 4
@@ -34,19 +52,20 @@ def test_egnn():
     
     z = egnn.apply(params, x)
     
-    # Test that flow results of two "equivalent" (under lattice translations of PBC)
-    # particle configurations are equivalent.
-    print("---- Test lattice translations of PBC ----")
-    image = np.random.randint(-5, 6, size=(n, dim)) * L
-    imagez = egnn.apply(params, x + image)
-    assert jnp.allclose(imagez, z + image)
-
+    # Test that flow results of two "equivalent"
     # Test the translation equivariance.
     print("---- Test translation equivariance ----")
     shift = jnp.array( np.random.randn(dim) )
     shiftz = egnn.apply(params, x + shift)
     assert jnp.allclose(shiftz, z + shift)
-    
+   
+    # Test the rotation equivariance
+    print("---- Test rotation equivariance ----")
+    angle = jax.random.uniform(key, (dim,), minval=0, maxval=2*jnp.pi)
+    rotatex = rotate(x, angle)
+    rotatez = egnn.apply(params, rotatex)
+    assert jnp.allclose(rotatez, rotate(z, angle))
+
     # Test of permutation equivariance.
     print("---- Test permutation equivariance ----")
     P = np.random.permutation(n)
